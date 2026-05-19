2c6621ed955ae718b0dcb2b32c538a1e69373eeb
jnorotsky <8887y7gw2s@privaterelay.appleid.com>
2026-05-19 16:41:16 -0700

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -12,15 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#f7f4ef",
-        }}
-      >
-        {children}
-      </body>
+      <body style={{ margin: 0, background: "#f7f4ef" }}>{children}</body>
     </html>
   );
 }
