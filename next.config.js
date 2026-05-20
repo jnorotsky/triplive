e1a65161c12308703ea86c24af4d43e987cf77cd
JENNIFER POLLAK <jenniferpollak@JENNIFERs-MacBook-Air.local>
2026-05-19 18:10:49 -0700

    Fix next.config.js serverExternalPackages key for Next.js 14.2+

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@vercel/postgres'],
-  },
+  serverExternalPackages: ['@vercel/postgres'],
 };
 
 module.exports = nextConfig;
